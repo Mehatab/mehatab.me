2f31710cbc1a7ea3224752c3f065290d9d05b347
Mehatab <shaikh.mehatab@gmail.com>
2020-04-28 23:21:23 +0530

    Updates

---
+++ precache-manifest.2393c0493b9dfb7b5065ba10abc138a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72a120e055a79c9ef0849247017bcbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e758638392c63e581ae",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7fee4168a652a2c4d3b0",
+    "url": "/static/js/2.74eef2b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74eef2b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e758638392c63e581ae",
+    "url": "/static/js/main.b96373a7.chunk.js"
+  },
+  {
+    "revision": "a98b6397007f54cd4920",
+    "url": "/static/js/runtime-main.f1f94eb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mehatab.me/precache-manifest.ad413739a150220f85ae1cf35200337c.js"
+  "/precache-manifest.2393c0493b9dfb7b5065ba10abc138a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mehatab.me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
