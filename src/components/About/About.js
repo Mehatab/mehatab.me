3135b02d8b467ddbb53a44ec923eb7857262bdbd
Mehatab <shaikh.mehatab@gmail.com>
2020-04-29 20:58:06 +0530

    update about description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ const About = ({ name }) => {
 			<div className="details">
 				<div className="item text-muted">
 					<p>
-						I develop Android apps for a living. Fortunately, It’s also something I’m really passionate about. When not working with my overlords, I spend time exploring Android and enjoy working on side projects.
+						I develop Android apps for a living. Fortunately, It’s also something I’m really passionate about. When not working with my overlords, I spend time hiking and taking photos of cool things I find.
 					</p>
 					<p>
 						Some of my previous works: &nbsp; &nbsp;
